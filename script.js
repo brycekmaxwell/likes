866dc2a984410131474696ba4bfab3fb62980433
Bryce Maxwell <brycemaxwell@Bryces-MBP.hitronhub.home>
2022-08-09 22:25:57 -0500

    new project

---
+++ script.js
@@ -0,0 +1,19 @@
+var like=0
+
+function addOne(){
+    like++;
+   document.querySelector('.neil-num').innerHTML=like;
+
+}
+var count=0
+function nicholeAdd(){
+    count++;
+    document.querySelector('.nichole-num').innerHTML=count;
+
+}
+var add=0
+function adding(){
+    add++;
+    document.querySelector('.number').innerHTML=add;
+
+}
